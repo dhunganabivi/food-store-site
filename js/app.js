b8c97f4fc74a1957047ece7395f8d0353a1912c4
Bivishan dhungana <58925886+dhunganabivi@users.noreply.github.com>
2022-12-10 11:47:35 +0545

    first time commiting

---
+++ js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.food-slider').slick({
+        autoplay: true,
+        slidesToShow:3,
+        prevArrow:".pre-btn",
+        nextArrow:".next-btn"
+    })    
+});
